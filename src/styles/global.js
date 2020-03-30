f4d1f8f82f9833225a95cd5c72c2e6b773ae2d1f
Rodrigo Carneiro <carneironline@gmail.com>
2020-03-30 00:58:01 -0300

    adicionar axios e images

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,15 +1,24 @@
 import { createGlobalStyle } from 'styled-components'
+import imgBackground from '../assets/images/background.svg'
 
 export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
     * {
         margin:0;
         padding:0;
         outline:0;
-        box-sizing:0;
+        box-sizing: border-box;
     }
 
     html, body, #root {
+        background: #191920 url(${imgBackground}) no-repeat;
         min-height:100%;
+    }
+
+    body, input, button {
+        font-family: Robot, sans-serif;
+        font-size:14px;
     }
 
     button {
